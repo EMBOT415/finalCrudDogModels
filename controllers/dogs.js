d421f90a4bb0581544e5dcabbb04dc9c9749c437
EMBOT415 <embot415@gmail.com>
2016-03-24 15:01:06 -0400

    click for details working

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -47,21 +47,21 @@ router.get('/seed', function(req, res){
 		rate: 700,
 		age: 8,
 		images:['http://res.cloudinary.com/embot415/image/upload/v1458837293/DSCN1736_mcft3o.jpg', 'http://res.cloudinary.com/embot415/image/upload/v1458830013/DSCN0136_evfitu.jpg', 'http://res.cloudinary.com/embot415/image/upload/v1458830013/DSCN0048_qyq4o0.jpg']
-},
+	},
 	{	name: 'Emma', 
 		breed: 'goldendoodle',
 		gender: 'female',
 		rate: 1500,
 		age: 10,
 		images:['http://res.cloudinary.com/embot415/image/upload/v1458838306/IMG_7688_zsz1lu.jpg', 'http://res.cloudinary.com/embot415/image/upload/v1458830001/IMG_7702_h9ei9u.jpg']
-},
+	},
 	{	name: 'Alfie', 
 		breed: 'tan dog',
 		gender: 'male',
 		rate: 1000,
 		age: 7,
 		images: ['http://res.cloudinary.com/embot415/image/upload/v1458837308/IMG_0361_hlkpoo.jpg', 'http://res.cloudinary.com/embot415/image/upload/v1458829174/IMG_1084_xubbfv.jpg', 'http://res.cloudinary.com/embot415/image/upload/v1458829167/IMG_1011_u8xj0d.jpg']
-},
+	},
 	{	name: 'Wilbur', 
 			breed: 'French Bulldog',
 			gender: 'male',
@@ -69,13 +69,27 @@ router.get('/seed', function(req, res){
 			age: 7,
 			images: ['http://res.cloudinary.com/embot415/image/upload/v1458841469/Screen_Shot_2016-03-24_at_1.38.49_PM_brc0mm.png', 'http://res.cloudinary.com/embot415/image/upload/v1458841469/Screen_Shot_2016-03-24_at_1.39.00_PM_aqu3rj.png', 'http://res.cloudinary.com/embot415/image/upload/v1458841471/Screen_Shot_2016-03-24_at_1.37.46_PM_ytpu8j.png', 'http://res.cloudinary.com/embot415/image/upload/v1458841470/Screen_Shot_2016-03-24_at_1.43.17_PM_yzjwg9.png', 'http://res.cloudinary.com/embot415/image/upload/v1458841470/Screen_Shot_2016-03-24_at_1.40.09_PM_ba42al.png']
 	},
+		{	name: 'Oliver Siegfried', 
+			breed: 'pom-a-poo',
+			gender: 'male',
+			rate: 1900,
+			age: 3,
+			images: ['http://res.cloudinary.com/embot415/image/upload/v1458842995/Screen_Shot_2016-03-24_at_2.06.43_PM_ek32au.png', 'http://res.cloudinary.com/embot415/image/upload/v1458842995/Screen_Shot_2016-03-24_at_2.06.43_PM_ek32au.png', 'http://res.cloudinary.com/embot415/image/upload/v1458842995/Screen_Shot_2016-03-24_at_2.04.48_PM_fpbsba.png', 'http://res.cloudinary.com/embot415/image/upload/v1458842996/Screen_Shot_2016-03-24_at_2.05.31_PM_ymf9hj.png']
+	},
+	{	name: 'Cash', 
+			breed: 'pit',
+			gender: 'female',
+			rate: 1700,
+			age: 3,
+			images: ['http://res.cloudinary.com/embot415/image/upload/v1458843368/Screen_Shot_2016-03-24_at_2.13.04_PM_drpuyz.png', 'http://res.cloudinary.com/embot415/image/upload/v1458843368/Screen_Shot_2016-03-24_at_2.12.45_PM_hpilvl.png', 'http://res.cloudinary.com/embot415/image/upload/v1458843368/Screen_Shot_2016-03-24_at_2.12.16_PM_qg4a6r.png', 'http://res.cloudinary.com/embot415/image/upload/v1458843368/Screen_Shot_2016-03-24_at_2.13.54_PM_fkemo0.png']
+	},
 	{	name: 'Wrigley', 
 		breed: 'norfolk terrier',
 		gender: 'male',
 		rate: 1000,
 		age: 8,
 		images: ['http://res.cloudinary.com/embot415/image/upload/v1458829173/IMG_1118_zpoatj.jpg', 'http://res.cloudinary.com/embot415/image/upload/v1458829159/IMG_1322_gujk3c.jpg']
-}	
+	}	
 		];
 			
 	// CREATE THE SEEDED USERS IN DB
